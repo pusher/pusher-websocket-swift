86ff509fab1e1e6d260d39a10a0c902268d04d9e
Hamilton Chapman <hamchapman@gmail.com>
2015-12-13 12:57:53 +0000

    Made PusherSwift.h umbrella header public

--- Sources/PusherSwift.h
+++ Sources/PusherSwift.h
@@ -13,7 +13,3 @@ FOUNDATION_EXPORT double PusherSwiftVersionNumber;
 
 //! Project version string for PusherSwift.
 FOUNDATION_EXPORT const unsigned char PusherSwiftVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <PusherSwift/PublicHeader.h>
-
-
