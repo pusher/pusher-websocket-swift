1a5beff050121744835a353a1e737fde134bcbc0
Hamilton Chapman <hamchapman@gmail.com>
2016-03-31 22:57:33 +0100

    Add tests for connection state change delegate

--- Tests/PusherSwiftTests.swift
+++ Tests/PusherSwiftTests.swift
@@ -176,6 +176,18 @@ public class MockPusherChannel: PusherChannel {
     }
 }
 
+public class TestConnectionStateChangeDelegate: ConnectionStateChangeDelegate {
+    let stubber = StubberForMocks()
+    
+    public func connectionChange(old: ConnectionState, new: ConnectionState) {
+        stubber.stub(
+            "connectionChange",
+            args: [old, new],
+            functionToCall: nil
+        )
+    }
+}
+
 public class StubberForMocks {
     public var calls:[FunctionCall]
     public var responses:[String:AnyObject]
@@ -903,6 +915,51 @@ class PusherPresenceChannelSpec: QuickSpec {
                 let chan = pusher.subscribe("presence-test") as? PresencePusherChannel
                 expect(chan?.members).toNot(beEmpty())
                 expect(chan?.members.first!.userInfo as? Dictionary<String, String>).to(equal(["twitter": "hamchapman"]))
+            }
+        }
+    }
+}
+
+class ConnectionStateChangeDelegateSpec: QuickSpec {
+    override func spec() {
+        var pusher: Pusher!
+        var socket: MockWebSocket!
+        var stateChangeDelegate: TestConnectionStateChangeDelegate!
+        
+        beforeEach({
+            pusher = Pusher(key: "key", options: ["autoReconnect": false])
+            socket = MockWebSocket()
+            stateChangeDelegate = TestConnectionStateChangeDelegate()
+            socket.delegate = pusher.connection
+            pusher.connection.socket = socket
+            pusher.connection.stateChangeDelegate = stateChangeDelegate
+        })
+        
+        describe("the delegate gets called") {
+            it("twice going from disconnected -> connecting -> connected") {
+                expect(pusher.connection.connectionState).to(equal(ConnectionState.Disconnected))
+                pusher.connect()
+                expect(pusher.connection.connectionState).to(equal(ConnectionState.Connected))
+                expect(stateChangeDelegate.stubber.calls.first?.name).to(equal("connectionChange"))
+                expect(stateChangeDelegate.stubber.calls.first?.args?.first as? ConnectionState).to(equal(ConnectionState.Disconnected))
+                expect(stateChangeDelegate.stubber.calls.first?.args?.last as? ConnectionState).to(equal(ConnectionState.Connecting))
+                expect(stateChangeDelegate.stubber.calls.last?.name).to(equal("connectionChange"))
+                expect(stateChangeDelegate.stubber.calls.last?.args?.first as? ConnectionState).to(equal(ConnectionState.Connecting))
+                expect(stateChangeDelegate.stubber.calls.last?.args?.last as? ConnectionState).to(equal(ConnectionState.Connected))
+            }
+
+            it("four times going from disconnected -> connecting -> connected -> disconnecting -> disconnected") {
+                expect(pusher.connection.connectionState).to(equal(ConnectionState.Disconnected))
+                pusher.connect()
+                expect(pusher.connection.connectionState).to(equal(ConnectionState.Connected))
+                pusher.disconnect()
+                expect(stateChangeDelegate.stubber.calls.count).to(equal(4))
+                expect(stateChangeDelegate.stubber.calls[2].name).to(equal("connectionChange"))
+                expect(stateChangeDelegate.stubber.calls[2].args?.first as? ConnectionState).to(equal(ConnectionState.Connected))
+                expect(stateChangeDelegate.stubber.calls[2].args?.last as? ConnectionState).to(equal(ConnectionState.Disconnecting))
+                expect(stateChangeDelegate.stubber.calls.last?.name).to(equal("connectionChange"))
+                expect(stateChangeDelegate.stubber.calls.last?.args?.first as? ConnectionState).to(equal(ConnectionState.Disconnecting))
+                expect(stateChangeDelegate.stubber.calls.last?.args?.last as? ConnectionState).to(equal(ConnectionState.Disconnected))
             }
         }
     }
