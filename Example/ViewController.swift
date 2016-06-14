25c8f3e5ea3fffbb2c940d9e0279fb3d16cd14d1
Hamilton Chapman <hamchapman@gmail.com>
2016-06-14 16:25:10 +0100

    Add more to example iOS app and change build settings to include the PusherSwift-iOS framework as opposed to the pod install framework.

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -14,20 +14,35 @@ class ViewController: UIViewController, ConnectionStateChangeDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // remove the debugLogger from the client options if you want to remove the
+        // debug logging, or just change the function below
+        let debugLogger = { (text: String) in debugPrint(text) }
+
         // Only use your secret here for testing or if you're sure that there's
         // no security risk
-        let pusher = Pusher(key: "YOUR_APP_KEY", options: ["secret": "YOUR_APP_SECRET"])
+        let pusher = Pusher(key: "YOUR_APP_KEY", options: ["secret": "YOUR_APP_SECRET", "debugLogger": debugLogger])
 
         pusher.connection.stateChangeDelegate = self
         pusher.connect()
 
+        pusher.bind({ (message: AnyObject?) in
+            if let message = message as? [String: AnyObject], eventName = message["event"] as? String where eventName == "pusher:error" {
+                if let data = message["data"] as? [String: AnyObject], errorMessage = data["message"] as? String {
+                    print("Error message: \(errorMessage)")
+                }
+            }
+        })
+
         let onMemberAdded = { (member: PresenceChannelMember) in
             print(member)
         }
+
         let chan = pusher.subscribe("presence-channel", onMemberAdded: onMemberAdded)
 
         chan.bind("test-event", callback: { (data: AnyObject?) -> Void in
             print(data)
+            pusher.subscribe("presence-channel", onMemberAdded: onMemberAdded)
+
             if let data = data as? Dictionary<String, AnyObject> {
                 if let testVal = data["test"] as? String {
                     print(testVal)
@@ -35,10 +50,12 @@ class ViewController: UIViewController, ConnectionStateChangeDelegate {
             }
         })
 
+        // triggers a client event
         chan.trigger("client-test", data: ["test": "some value"])
     }
 
     func connectionChange(old: ConnectionState, new: ConnectionState) {
+        // print the old and new connection states
         print("old: \(old) -> new: \(new)")
     }
 }
