c4bd6b4e001053d30bb5b99990c80547f3cb7efa
Blair McArthur <blair.mcarthur@skipthedishes.com>
2016-06-14 16:39:46 -0500

    made the port option non-optional and defaulted it within the PusherClientOptions.

--- Source/PusherSwift.swift
+++ Source/PusherSwift.swift
@@ -126,11 +126,9 @@ func constructUrl(key: String, options: PusherClientOptions) -> String {
     var url = ""
 
     if options.encrypted {
-        let defaultPort = (options.port ?? 443)
-        url = "wss://\(options.host):\(defaultPort)/app/\(key)"
+        url = "wss://\(options.host):\(options.port)/app/\(key)"
     } else {
-        let defaultPort = (options.port ?? 80)
-        url = "ws://\(options.host):\(defaultPort)/app/\(key)"
+        url = "ws://\(options.host):\(options.port)/app/\(key)"
     }
     return "\(url)?client=\(CLIENT_NAME)&version=\(VERSION)&protocol=\(PROTOCOL)"
 }
