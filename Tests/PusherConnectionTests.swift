c96486d8ff62aeb7f33bc6b1e1b16f800c217e79
Hamilton Chapman <hamchapman@gmail.com>
2016-06-21 16:39:43 +0100

    Add PusherConnectionTests (with commented out tests) to replace the tests removed from the client initialisation tests

--- Tests/PusherConnectionTests.swift
+++ Tests/PusherConnectionTests.swift
@@ -7,3 +7,44 @@
 //
 
 import Foundation
+
+
+//                it("has userDataFetcher as nil") {
+//                    expect(pusher.connection.options.userDataFetcher).to(beNil())
+//                }
+//
+//                it("has authRequestCustomizer as nil") {
+//                    expect(pusher.connection.options.authRequestCustomizer).to(beNil())
+//                }
+//
+//                it("has debugLogger set as nil") {
+//                    expect(pusher.connection.options.debugLogger).to(beNil())
+//                }
+//
+//                context("an authRequestCustomizer") {
+//                    it("has one set") {
+//                        func customizer(request: NSMutableURLRequest) -> NSMutableURLRequest {
+//                            return request
+//                        }
+//                        pusher = Pusher(key: key, options: ["authRequestCustomizer": customizer])
+//                        expect(pusher.connection.options.authRequestCustomizer).toNot(beNil())
+//                    }
+//                }
+//
+//                context("a debugLogger") {
+//                    it("sets the debugLogger with it") {
+//                        let debugLogger = { (text: String) in }
+//                        pusher = Pusher(key: key, options: ["debugLogger": debugLogger])
+//                        expect(pusher.connection.options.debugLogger).toNot(beNil())
+//                    }
+//                }
+//
+//                context("a userDataFetcher function") {
+//                    it("has one function set") {
+//                        func fetchFunc() -> PusherUserData {
+//                            return PusherUserData(userId: "1")
+//                        }
+//                        pusher = Pusher(key: key, options: ["userDataFetcher": fetchFunc])
+//                        expect(pusher.connection.options.userDataFetcher).toNot(beNil())
+//                    }
+//                }
