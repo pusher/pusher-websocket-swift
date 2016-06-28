0e0f6b8c4d5958750afb384abf72900f3081f071
DrAL3X <alessandro.calzavara@gmail.com>
2016-06-28 15:54:05 +0200

    fixed attemptToReturnJSONObject option when it's false

--- Source/PusherChannel.swift
+++ Source/PusherChannel.swift
@@ -84,7 +84,7 @@ public class PusherChannel {
     */
     public func handleEvent(eventName: String, eventData: String) {
         if let eventHandlerArray = self.eventHandlers[eventName] {
-            if let _ = connection.options.attemptToReturnJSONObject {
+            if let attempt = connection.options.attemptToReturnJSONObject where attempt {
                 for eventHandler in eventHandlerArray {
                     eventHandler.callback(connection.getEventDataJSONFromString(eventData))
                 }
