9610f7736d90b48b016657e2c07d11628e08053a
Hamilton Chapman <hamchapman@gmail.com>
2016-07-07 11:49:42 +0100

    Tidied up AppDelegate in iOS Example

--- Example/AppDelegate.swift
+++ Example/AppDelegate.swift
@@ -16,17 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let pusher = Pusher(key: "YOUR_PUSHER_APP_KEY")
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
-
-        pusher.connect()
-
-        let channel = pusher.subscribe("test-channel")
-        channel.bind("yolo", callback: { (data : AnyObject?) -> Void in
-            print(data)
-        })
-
-
-        // Override point for customization after application launch.
         let notificationTypes : UIUserNotificationType = [UIUserNotificationType.Alert, UIUserNotificationType.Badge, UIUserNotificationType.Sound]
         let pushNotificationSettings = UIUserNotificationSettings(forTypes: notificationTypes, categories: nil)
         application.registerUserNotificationSettings(pushNotificationSettings)
@@ -69,7 +58,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
-
