19f0c17a37e639091ea7d64b9d8c7e4ae3c5af3e
Hamilton Chapman <hamchapman@gmail.com>
2017-12-11 11:27:05 +0000

    Aesthetic tweaks

--- Sources/AuthRequestBuilderProtocol.swift
+++ Sources/AuthRequestBuilderProtocol.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 @objc public protocol AuthRequestBuilderProtocol {
+    @objc optional func requestFor(socketID: String, channelName: String) -> URLRequest?
+
     @available(*, deprecated: 4.0.2, message: "use requestFor(socketID: String, channelName: String) -> URLRequest? instead")
     @objc optional func requestFor(socketID: String, channel: PusherChannel) -> NSMutableURLRequest?
-
-    @objc optional func requestFor(socketID: String, channelName: String) -> URLRequest?
 }
