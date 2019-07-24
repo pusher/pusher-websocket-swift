366dfc577c7e8665831771fe255f6afd50b7da31
jd@jonathandowning.uk <jd@jonathandowning.uk>
2019-07-24 15:57:49 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,6 +1,26 @@
+// swift-tools-version:5.0
+
 import PackageDescription
 
 let package = Package(
     name: "PusherSwift",
-    exclude: ["Tests"]
+    products: [
+        .library(name: "PusherSwift", targets: ["PusherSwift"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.0.0"),
+        .package(url: "https://github.com/ashleymills/Reachability.swift.git", from: "4.3.1"),
+        .package(url: "https://github.com/daltoniam/Starscream.git", from: "3.1.0"),
+    ],
+    targets: [
+        .target(
+            name: "PusherSwift",
+            dependencies: [
+                "CryptoSwift",
+                "Reachability",
+                "Starscream",
+            ],
+            path: "Sources"
+        )
+    ]
 )
