e24a95771f187361fce68bc1963b14d28ffb63e2
Tom Kemp <tom.kemp@pusher.com>
2020-02-18 12:29:39 +0000

    Remove redundant nil check

--- Tests/PusherCryptoTest.swift
+++ Tests/PusherCryptoTest.swift
@@ -8,7 +8,6 @@ class PusherCryptoTest: XCTestCase {
         let message = "{\"user\":\"my user data\"}"
 
         let digest = PusherCrypto.generateSHA256HMAC(secret: secret, message: message)
-        XCTAssertNotNil(digest)
 
         let expectedDigest = "7705bb9a7934fe4ceee2325e23750f35752899448c2fe5b064d93326c98fd5b3"
         XCTAssertEqual(digest, expectedDigest)
