3f12623a92e45d3beb8968178c92ea333dd65609
Tom Kemp <tom.kemp@pusher.com>
2020-04-16 16:23:42 +0100

    Add EventQueue tests

--- Tests/PusherSwiftWithEncryption-Only/PusherEventQueue+DecryptionTests.swift
+++ Tests/PusherSwiftWithEncryption-Only/PusherEventQueue+DecryptionTests.swift
@@ -63,13 +63,240 @@ class PusherEventQueueDecryptionTests: XCTestCase {
         wait(for: [ex], timeout: 0.5)
     }
 
+    func testShouldReloadDecryptionKeyAndDecryptSuccessfully() {
 
+        let wrongDecryptionKey = "00000000000000000000000000000000000000000000"
+        let correctDecryptionKey = "EOWC/ked3NtBDvEs9gFwk7x4oZEbH9I0Lz2qkopBxxs="
 
-    // Need encrypted version too
-    // Valid encrypted event without channel name - valid key
-    // Valid encrypted event with channel name - valid key
+        let dataPayload = """
+        {
+          "nonce": "Ew2lLeGzSefk8fyVPbwL1yV+8HMyIBrm",
+          "ciphertext": "ig9HfL7OKJ9TL97WFRG0xpuk9w0DXUJhLQlQbGf+ID9S3h15vb/fgDfsnsGxQNQDxw+i"
+        }
+        """.removing(.whitespacesAndNewlines)
 
-    // Should call reloadDecryption for a sequence of wrong events
-    // Should only call reloadDecryption once if subsequent messages can be decrypted
-    // If can't decrypt message after reloading, should call didFailToDecryptEventWithPayload and move on
+        let jsonDict = """
+        {
+          "event": "user-event",
+          "channel": "private-encrypted-channel",
+          "data": \(dataPayload.escaped)
+        }
+        """.toJsonDict()
+
+        let expectedDecryptedPayload = """
+        {
+          "name": "freddy",
+          "message": "hello"
+        }
+        """.removing(.whitespacesAndNewlines)
+
+        keyProvider.setDecryptionKey(wrongDecryptionKey, forChannelName: "private-encrypted-channel")
+
+        let reloadEx = expectation(description: "should attempt to reload key")
+        let receivedEv = expectation(description: "should call didReceiveEvent")
+
+        eventQueueDelegate.reloadDecryptionKeySync = { (eventQueue, channelName) in
+            XCTAssertEqual("private-encrypted-channel", channelName)
+            self.keyProvider.setDecryptionKey(correctDecryptionKey, forChannelName: channelName)
+            reloadEx.fulfill()
+        }
+
+        eventQueueDelegate.didReceiveEvent = { (eventQueue, event, channelName) in
+            XCTAssertEqual(event.data, expectedDecryptedPayload)
+            XCTAssertEqual("private-encrypted-channel", channelName)
+            receivedEv.fulfill()
+        }
+
+        eventQueue.report(json: jsonDict, forChannelName: "private-encrypted-channel")
+        wait(for: [reloadEx, receivedEv], timeout: 0.5)
+    }
+
+    func testShouldReloadDecryptionKeyOnceAndFailIfSecondKeyIsBad() {
+        let wrongDecryptionKey0 = "00000000000000000000000000000000000000000000"
+        let wrongDecryptionKey1 = "11111111111111111111111111111111111111111111"
+
+        let dataPayload = """
+        {
+          "nonce": "Ew2lLeGzSefk8fyVPbwL1yV+8HMyIBrm",
+          "ciphertext": "ig9HfL7OKJ9TL97WFRG0xpuk9w0DXUJhLQlQbGf+ID9S3h15vb/fgDfsnsGxQNQDxw+i"
+        }
+        """.removing(.whitespacesAndNewlines)
+
+        let jsonDict = """
+        {
+          "event": "user-event",
+          "channel": "private-encrypted-channel",
+          "data": \(dataPayload.escaped)
+        }
+        """.toJsonDict()
+
+        keyProvider.setDecryptionKey(wrongDecryptionKey0, forChannelName: "private-encrypted-channel")
+
+        let reloadEx = expectation(description: "should attempt to reload key")
+        let failedEv = expectation(description: "should fail to decrypt message")
+
+        eventQueueDelegate.reloadDecryptionKeySync = { (eventQueue, channelName) in
+            XCTAssertEqual("private-encrypted-channel", channelName)
+            self.keyProvider.setDecryptionKey(wrongDecryptionKey1, forChannelName: channelName)
+            reloadEx.fulfill()
+        }
+
+        eventQueueDelegate.didFailToDecryptEvent = { (event, payload, channelName) in
+            let equal = NSDictionary(dictionary: jsonDict).isEqual(to: payload)
+            XCTAssertTrue(equal)
+            XCTAssertEqual("private-encrypted-channel", channelName)
+            failedEv.fulfill()
+        }
+
+        eventQueue.report(json: jsonDict, forChannelName: "private-encrypted-channel")
+        wait(for: [reloadEx, failedEv], timeout: 0.5)
+    }
+
+    func testShouldMoveOnAfterFailingToDecryptAMessage() {
+        let wrongDecryptionKey = "00000000000000000000000000000000000000000000"
+
+        // Decryption key for "decryptableData" but not "undecryptableData"
+        let correctDecryptionKey = "EOWC/ked3NtBDvEs9gFwk7x4oZEbH9I0Lz2qkopBxxs="
+
+        let undecryptableData = """
+        {
+          "nonce": "7w2hU5r5VMj3PGXXepgP6E/KgPob5o6t",
+          "ciphertext": "FX0lJZu33f0dWPb89816ngn0l9NfJC5mFny6EQF6z25K+Ly5LFS9hP7XAC6s5pUoZqGXzC03FA=="
+        }
+        """.removing(.whitespacesAndNewlines)
+
+        let decryptableData = """
+        {
+          "nonce": "Ew2lLeGzSefk8fyVPbwL1yV+8HMyIBrm",
+          "ciphertext": "ig9HfL7OKJ9TL97WFRG0xpuk9w0DXUJhLQlQbGf+ID9S3h15vb/fgDfsnsGxQNQDxw+i"
+        }
+        """.removing(.whitespacesAndNewlines)
+
+        let generateEvent = { (payload: String) in
+            return """
+            {
+              "event": "user-event",
+              "channel": "private-encrypted-channel",
+              "data": \(payload.escaped)
+            }
+            """.toJsonDict()
+        }
+
+        let undecryptableEvent = generateEvent(undecryptableData)
+        let decryptableEvent = generateEvent(decryptableData)
+
+        let expectedDecryptedPayload = """
+        {
+          "name": "freddy",
+          "message": "hello"
+        }
+        """.removing(.whitespacesAndNewlines)
+
+        keyProvider.setDecryptionKey(wrongDecryptionKey, forChannelName: "private-encrypted-channel")
+
+        let reloadEx = expectation(description: "should attempt to reload key")
+        let failedEv = expectation(description: "should fail to decrypt message")
+        let successEv = expectation(description: "should succeed in decrypting message")
+
+        eventQueueDelegate.reloadDecryptionKeySync = { (eventQueue, channelName) in
+            XCTAssertEqual("private-encrypted-channel", channelName)
+            self.keyProvider.setDecryptionKey(correctDecryptionKey, forChannelName: channelName)
+            reloadEx.fulfill()
+        }
+
+        eventQueueDelegate.didFailToDecryptEvent = { (event, payload, channelName) in
+            let equal = NSDictionary(dictionary: undecryptableEvent).isEqual(to: payload)
+            XCTAssertTrue(equal)
+            XCTAssertEqual("private-encrypted-channel", channelName)
+            failedEv.fulfill()
+        }
+
+        eventQueueDelegate.didReceiveEvent = { (eventQueue, event, channelName) in
+            XCTAssertEqual(expectedDecryptedPayload, event.data)
+            XCTAssertEqual("private-encrypted-channel", channelName)
+            successEv.fulfill()
+        }
+
+        eventQueue.report(json: undecryptableEvent, forChannelName: "private-encrypted-channel")
+        eventQueue.report(json: decryptableEvent, forChannelName: "private-encrypted-channel")
+
+        wait(for: [reloadEx, failedEv, successEv], timeout: 0.5)
+    }
+
+    func testFailingToDecryptOnOneChannelShouldNotAffectAnother() {
+        let decryptableChannel = "private-encrypted-decryptable"
+        let undecryptableChannel = "private-encrypted-fail"
+
+        let wrongDecryptionKey = "00000000000000000000000000000000000000000000"
+
+        // Decryption key for "decryptableData" but not "undecryptableData"
+        let correctDecryptionKey = "EOWC/ked3NtBDvEs9gFwk7x4oZEbH9I0Lz2qkopBxxs="
+
+        let undecryptableData = """
+        {
+          "nonce": "7w2hU5r5VMj3PGXXepgP6E/KgPob5o6t",
+          "ciphertext": "FX0lJZu33f0dWPb89816ngn0l9NfJC5mFny6EQF6z25K+Ly5LFS9hP7XAC6s5pUoZqGXzC03FA=="
+        }
+        """.removing(.whitespacesAndNewlines)
+
+        let decryptableData = """
+        {
+          "nonce": "Ew2lLeGzSefk8fyVPbwL1yV+8HMyIBrm",
+          "ciphertext": "ig9HfL7OKJ9TL97WFRG0xpuk9w0DXUJhLQlQbGf+ID9S3h15vb/fgDfsnsGxQNQDxw+i"
+        }
+        """.removing(.whitespacesAndNewlines)
+
+        let generateEvent = { (channel: String, payload: String) in
+            return """
+            {
+              "event": "user-event",
+              "channel": "\(channel)",
+              "data": \(payload.escaped)
+            }
+            """.toJsonDict()
+        }
+
+        let undecryptableEvent = generateEvent(undecryptableChannel, undecryptableData)
+        let decryptableEvent = generateEvent(decryptableChannel, decryptableData)
+
+        let expectedDecryptedPayload = """
+        {
+          "name": "freddy",
+          "message": "hello"
+        }
+        """.removing(.whitespacesAndNewlines)
+
+        keyProvider.setDecryptionKey(correctDecryptionKey, forChannelName: decryptableChannel)
+        keyProvider.setDecryptionKey(wrongDecryptionKey, forChannelName: undecryptableChannel)
+
+        let reloadEx = expectation(description: "should attempt to reload key")
+        let failedEv = expectation(description: "should fail to decrypt message")
+        let successEv = expectation(description: "should succeed in decrypting message")
+
+        eventQueueDelegate.reloadDecryptionKeySync = { (eventQueue, channelName) in
+            XCTAssertEqual(undecryptableChannel, channelName)
+            self.keyProvider.setDecryptionKey(wrongDecryptionKey, forChannelName: undecryptableChannel)
+            reloadEx.fulfill()
+        }
+
+        eventQueueDelegate.didFailToDecryptEvent = { (event, payload, channelName) in
+            let equal = NSDictionary(dictionary: undecryptableEvent).isEqual(to: payload)
+            XCTAssertTrue(equal)
+            XCTAssertEqual(undecryptableChannel, channelName)
+            failedEv.fulfill()
+        }
+
+        eventQueueDelegate.didReceiveEvent = { (eventQueue, event, channelName) in
+            XCTAssertEqual(expectedDecryptedPayload, event.data)
+            XCTAssertEqual(decryptableChannel, channelName)
+            successEv.fulfill()
+        }
+
+        eventQueue.report(json: undecryptableEvent, forChannelName: undecryptableChannel)
+        eventQueue.report(json: decryptableEvent, forChannelName: decryptableChannel)
+
+        wait(for: [reloadEx, failedEv, successEv], timeout: 0.5)
+    }
+
+    
 }
