341dccfc37649f0be638918813efd6807f3138d8
Tom Kemp <tom.kemp@pusher.com>
2020-04-20 10:33:17 +0100

    Fix warning

--- Sources/PusherSwift.swift
+++ Sources/PusherSwift.swift
@@ -51,18 +51,7 @@ let CLIENT_NAME = "pusher-websocket-swift"
         onMemberRemoved: ((PusherPresenceChannelMember) -> ())? = nil
     ) -> PusherChannel {
 
-        #if !WITH_ENCRYPTION
-        if PusherEncryptionHelpers.isEncryptedChannel(channelName: channelName) {
-            let error = """
-
-            WARNING: You are subscribing to an encrypted channel: '\(channelName)' but this version of PusherSwift does not \
-            support end-to-end encryption. Events will not be decrypted. You must import 'PusherSwiftWithEncryption' in \
-            order for events to be decrypted. See https://github.com/pusher/pusher-websocket-swift for more information
-
-            """
-            print(error)
-        }
-        #endif
+        PusherDecryptor.encryptedChannelWarning(forChannelName: channelName)
 
         return self.connection.subscribe(
             channelName: channelName,
