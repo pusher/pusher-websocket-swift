e9b5bc294b29c6e1100feebe7b7fd816158455a1
Roberto Casula <roberto.casula@bitapp.it>
2020-10-06 10:46:43 +0200

    Fix depl target

--- Package.swift
+++ Package.swift
@@ -1,14 +1,20 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 
 import PackageDescription
 
 let package = Package(
     name: "PusherSwift",
+    platforms: [
+        .iOS(.v13),
+//        .watchOS(.v6),
+//        .tvOS(.v13),
+//        .macOS(.v10_15)
+    ],
     products: [
         .library(name: "PusherSwift", targets: ["PusherSwift"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ashleymills/Reachability.swift.git", .upToNextMajor(from: "5.0.0")),
+        .package(name: "Reachability", url: "https://github.com/ashleymills/Reachability.swift.git", .upToNextMajor(from: "5.0.0")),
         .package(url: "https://github.com/daltoniam/Starscream.git", .upToNextMajor(from: "3.1.0")),
     ],
     targets: [
