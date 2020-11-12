bfaecd11cf1e0c6644ffa6a34abc98214c6dd38e
Daniel Browne <daniel.browne@pusher.com>
2020-11-12 15:18:38 +0000

    Update Consumption-Tests for TweetNacl and tvOS support

--- Consumption-Tests/Shared/performTests.sh
+++ Consumption-Tests/Shared/performTests.sh
@@ -100,13 +100,6 @@ function runXcodeBuild {
 	local SCHEME="$2"
 	echo "SCHEME=$SCHEME"
 
-	if [[ "$SCHEME" == *"tvOS-WithEncryption" ]];	then
-		SUMMARY_LOG_OUTPUT+="\n 🔘 $SCHEME (tvOS is not supported with PusherSwiftWithEncryption)"
-		echo "**** SKIPPING '$NAME - $SCHEME' ****"	
-		echo "------ END: $FUNCNAME $@ ------"
-		return 0
-	fi
-
 	if [[ "$NAME" == "SwiftPackageManager"* ]] && [[ "$SCHEME" == *"WithoutEncryption" ]];	then
 		SUMMARY_LOG_OUTPUT+="\n 🔘 $SCHEME (SPM integration does not expose PusherSwift module)"
 		echo "**** SKIPPING '$NAME - $SCHEME' ****"	
