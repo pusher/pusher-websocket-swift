e97dd18624fd4a9d4e197d7ea596994072196b0a
Daniel Browne <danbrowne@pusher.com>
2020-11-25 15:36:17 +0000

    Rename targets in 'Package.swift'

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "PusherSwift",
     platforms: [.iOS("13.0"), .macOS("10.15"), .tvOS("13.0")],
     products: [
-        .library(name: "PusherSwift", targets: ["PusherSwiftWithEncryption"])
+        .library(name: "PusherSwift", targets: ["PusherSwift"])
     ],
     dependencies: [
         .package(url: "https://github.com/pusher/NWWebSocket.git", .upToNextMajor(from: "0.5.0")),
@@ -14,7 +14,7 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "PusherSwiftWithEncryption",
+            name: "PusherSwift",
             dependencies: [
                 "NWWebSocket",
                 "TweetNacl",
@@ -22,8 +22,8 @@ let package = Package(
             path: "Sources"
         ),
         .testTarget(
-            name: "PusherSwiftWithEncryptionTests",
-            dependencies: ["PusherSwiftWithEncryption"],
+            name: "PusherSwiftTests",
+            dependencies: ["PusherSwift"],
             path: "Tests",
             swiftSettings: [.define("WITH_ENCRYPTION")]
         )
