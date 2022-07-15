35b8810b1282b9cb4ff073cf64f4d27e2ab112ec
fbenevides <me@benevides.pro>
2022-07-15 15:00:56 +0200

    Release 10.1.0

--- Sources/PusherSwift.swift
+++ Sources/PusherSwift.swift
@@ -2,7 +2,7 @@ import Foundation
 import NWWebSocket
 
 let PROTOCOL = 7
-let VERSION = "10.0.1"
+let VERSION = "10.1.0"
 // swiftlint:disable:next identifier_name
 let CLIENT_NAME = "pusher-websocket-swift"
 
